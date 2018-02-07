0a25a3e61143935554d1d07f45880ade26e15d45
Nuno Neves <youknownuno@gmail.com>
2018-02-07 11:05:12 -0800

    changed the name of the old schema.sql to schema_carn.sql

---
+++ database/schema_carn.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE carn;
+\c carn;
+
+CREATE TABLE passengers (
+  id SERIAL PRIMARY KEY,
+  zip INT NOT NULL,
+  lat DECIMAL(9,7) NOT NULL,
+  lon DECIMAL(10,7) NOT NULL,
+  dest_zip INT NOT NULL,
+  dest_lat DECIMAL(9,7) NOT NULL,
+  dest_lon DECIMAL(10,7) NOT NULL,
+  last_seen TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  driver_id INT NOT NULL,
+  loc_zip INT NOT NULL,
+  loc_lat DECIMAL(9,7) NOT NULL,
+  loc_lon DECIMAL(10,7) NOT NULL,
+  dest_zip INT NOT NULL,
+  dest_lat DECIMAL(9,7) NOT NULL,
+  dest_lon DECIMAL(10,7) NOT NULL,
+  rate INT NOT NULL,
+  price DECIMAL(6,2) NOT NULL,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
