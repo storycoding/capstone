f65f352bd355ece9fb4f10416800b7bd75ef4533
Nuno Neves <youknownuno@gmail.com>
2018-02-08 11:46:29 -0800

    optimized generateData.js

--- database/generateData.js
+++ database/generateData.js
@@ -2,34 +2,36 @@ const saveBooking = require('./queries.js').saveBooking;
 const saveRide = require('./queries.js').saveRide;
 const saveLogin = require('./queries.js').saveLogin;
 
+//[37.783783, -122.4114007]
+
 const zips = {
-  94102: [37.783783, -122.4114007],
-  94103: [37.7793322, -122.4341587],
-  94104: [37.793186, -122.4036837],
-  94105: [37.788722, -122.3985992],
-  94107: [37.7556837, -122.4021276],
-  94108: [37.7909771, -122.4179006],
-  94109: [37.805056, -122.4281884],
-  94110: [37.7486943, -122.4325716],
-  94111: [37.7991221, -122.4062223],
-  94112: [37.7215472, -122.4598041],
-  94114: [37.7616271, -122.4374621],
-  94115: [37.7861432, -122.4556077],
-  94116: [37.7483257, -122.4989142],
-  94117: [37.7687991, -122.4627462],
-  94118: [37.7788482, -122.4791112],
-  94121: [37.7772111, -122.5123031],
-  94122: [37.7637942, -122.4711964],
-  94123: [37.8007141, -122.4565512],
-  94124: [37.717172, -122.3965978],
-  94127: [37.7364347, -122.4750097],
-  94129: [37.7989022, -122.4841773],
-  94131: [37.7461683, -122.4523647],
-  94132: [37.7218297, -122.5029057],
-  94133: [37.8069357, -122.415765],
-  94134: [37.71795, -122.424504],
-  94143: [37.764281, -122.4579149],
-  94158: [37.7674991, -122.3934175]
+  94102: [783783, 4114007],
+  94103: [7793322, 4341587],
+  94104: [793186, 4036837],
+  94105: [788722, 3985992],
+  94107: [7556837, 4021276],
+  94108: [7909771, 4179006],
+  94109: [805056, 4281884],
+  94110: [7486943, 4325716],
+  94111: [7991221, 4062223],
+  94112: [7215472, 4598041],
+  94114: [7616271, 4374621],
+  94115: [7861432, 4556077],
+  94116: [7483257, 4989142],
+  94117: [7687991, 4627462],
+  94118: [7788482, 4791112],
+  94121: [7772111, 5123031],
+  94122: [7637942, 4711964],
+  94123: [8007141, 4565512],
+  94124: [717172, 3965978],
+  94127: [7364347, 4750097],
+  94129: [7989022, 4841773],
+  94131: [7461683, 4523647],
+  94132: [7218297, 5029057],
+  94133: [8069357, 415765],
+  94134: [71795, 424504],
+  94143: [764281, 4579149],
+  94158: [7674991, 3934175]
 }
 
 const randomRate = () => {
@@ -68,10 +70,10 @@ const generateLocation = (code) => {
 
 
 const generateBooking = (users) => {
-  console.log(`current iteration: ${i}`)
-  let streams = {};
+  console.log(`current iteration: ${i} / 200`);
 
-  for (let j = start; j < users; j++) {
+  let end = users + increment;
+  for (let j = users; j < end; j++) {
 
     if(id > 10000000) { return }
 
@@ -84,10 +86,10 @@ const generateBooking = (users) => {
     for (let k = 0; k < 5; k++) {
       let driver_id = randomDriver();
 
-      let loc_zip = dest_zip || randomZip();
-      let loc = dest_zip || generateLocation(loc_zip);
-      let loc_lat = dest_lat || loc[0];
-      let loc_lon = dest_lon || loc[1];
+      let loc_zip =  randomZip();
+      let loc =   generateLocation(loc_zip);
+      let loc_lat =  loc[0];
+      let loc_lon =  loc[1];
 
       let dest_zip = randomZip();
       let dest = generateLocation(dest_zip);
@@ -106,43 +108,33 @@ const generateBooking = (users) => {
       let small_loc_zip = loc_zip - 94100;
       let small_dest_zip = dest_zip - 94100;
 
-      saveRide(id, small_loc_zip, loc_lat, loc_lon, small_dest_zip, dest_lat, dest_lon, driver_id, user_id);
-
-      saveBooking(user_id, price, date_ms);
-
-      //user stays at the last booking location & date_ms
-      if (k === 4) {
-        user_zip = small_dest_zip;
-        user_lat = dest_lat;
-        user_lon = dest_lon;
-        user_date_ms = date_ms;
-
-        saveLogin(user_id, user_zip, user_lat, user_lon, user_date_ms);
+      console.log(`queued saveRide id:${id}`);
+      saveRide(id, small_loc_zip, loc_lat, loc_lon, small_dest_zip, dest_lat, dest_lon, driver_id).then(()=>{console.log(`wrote saveRide id:${id}`)}
+        );
       }
 
+      console.log(`queued saveBooking id:${id}`);
+      saveBooking(user_id, price, date_ms).then(()=>{console.log(`queued saveBooking id:${id}`)});
+      
       id ++;
     }
-
-    id ++;    
   }
-    
-  console.log(`finished queueing: ${i}`)
 
+  users += increment;
   i++;
 
-  start += increment;
-  users += increment;
-
   if (id < 10000000) {
-    setTimeout(function(){ generateBooking(users); }, 45000);
+    setTimeout(function(){ generateBooking(users) }, 45000);
 
-  } else { return }
+  } else { 
+    console.log("10M writes successfully queued to the database");
+    return;
+  }
 }
 
+let i = 1;
+let id = 1;
+let users = 1;
 let increment = 10000;
-let start = 0;
-let i = 0;
-let id = 0;
-let users = 10000;
 
 generateBooking(users)
