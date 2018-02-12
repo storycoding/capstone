e21614d137eb1dcf69c58fd39174dabe0fc1b759
Nuno Neves <youknownuno@gmail.com>
2018-02-11 19:34:47 -0800

    added Istanbul tester, tuned the mocha test

--- test/testAPI.js
+++ test/testAPI.js
@@ -1,60 +1,82 @@
-var chai = require('chai'); // for node testing
+var chai = require('chai');
 var query = require('../database/queries.js');
 var axios = require('axios');
 
 const assert = chai.assert;
 const should = chai.should();
 
-describe('Write to the database', function() {
-  it('should throw a success message upon making a write request to the database', function() {
+describe('queries.saveBooking', () => {
 
-   	query.saveBooking(20000, 1971, 1517955452).then(query.getBooking('id', 100005).then((result)=>{
-   		console.log(result);
+  it('should write to the bookings table', () => {
+   	  query.saveBooking(20000, 1971, 1517955452)
+        .then(query.getBooking('id', 100005)
+          .then((result) => {
+            //console.log("typeof result = ", result);
+      	    result.should.be.a('object');
+          })
+        );
+    });
+
+});
+
+
+describe('queries.getBooking', () => {
+
+  it('should return an object from the database', () => {
+    query.getBooking('id', 1).then((result) => {
     	result.should.be.a('object');
-    })
-    );
-    
+    });
+  });
+
+  it('should return multiple entries from a single user_id', () => {
+    query.getBooking('user_id', 1)
+      .then((result) => {
+        //console.log('result = ', result);
+    	  result.rows.should.have.lengthOf(5);
+      });
   });
 });
 
-describe('Read from database', function() {
-  it('should return an object from the database', function() {
 
-    query.getBooking('id', 1).then((result)=>{
-    	result.should.be.a('object');
+describe('queries.saveRide', () => {
+
+  it('should write to the rides table', () => {
+      query.saveRide(17, 7687991, 4627462, 31, 7461683, 4523647, 5069788)
+        .then(query.getRide('id', 17)
+          .then((result) => {
+            //console.log('getRide after saveRide = ', result)
+            result.should.be.a('object');
+          })
+        );
     });
-    
-  });
-
-  it('should return multiple entries from a single user_id', function() {
-
-    query.getBooking('user_id', 1).then((result)=>{
-    	//console.log(result.rows);
-    	result.rows.should.have.lengthOf(5);
-    });
-    
-  });
 });
 
-describe('Get data from the server', function() {
-  it('should receive an entry from the server upon sending a GET request', function() {
 
+describe('queries.getRide', () => {
+
+  it('should return an object from the bookings table', () => {
+    query.getRide('id', 1).then((result) => {
+      //console.log("getRide result = ", result)
+      result.should.be.a('object');
+    });
+  });
+
+});
+
+
+describe('Server', () => {
+
+  it('GET request should return an entry from the server', () => {
    	axios.get('http://127.0.0.1:2000/')
-	  .then(function (response, err) {
+	  .then((response, err) => {
 	  	response.should.be.a('object');
-	  	//console.log("response.data =", response.data);
 	  })
-	  .catch(function (error) {
-	    //console.log(error);
+	  .catch((error) => {
+	    console.error(error);
 	  });
     
   });
 });
 
+
 setTimeout(()=>{process.exit('process ended')}, 5000);
-
-// test a query to the database
-	// test a write to the database
-	// test a read after the write
-
-// test an api call to the server
